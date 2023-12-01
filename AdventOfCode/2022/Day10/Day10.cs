cb71b13e8bfc334e68442c1159e6ed5b78655ef2
HarrySimpson-KDOR <caniflysouthtoo@gmail.com>
2023-11-30 18:10:47 -0600

    still missing 10, 11, 12 will try to work them while I start 2023

---
+++ AdventOfCode/2022/Day10/Day10.cs
@@ -0,0 +1,72 @@
+﻿namespace AdventOfCode.Year2022
+{
+    public class Day10 : Day
+    {
+
+        public Day10(int today) : base(today)
+        {
+            
+        }
+        public override void RunPart1(ArgumentType argumentType)
+        {
+            string[] data = argumentType == ArgumentType.Sample ? Sample : Full;
+            CRTCPU cpu = new CRTCPU();
+            result = cpu.GetSignal(data).ToString();
+            Console.WriteLine(result);
+        }
+        public override void RunPart2(ArgumentType argumentType)
+        {
+            string[] data = argumentType == ArgumentType.Sample ? Sample : Full;
+            result = "";
+            Console.WriteLine(result);
+        }
+        public class CRTCPU
+        {
+            int reg1 = 1;
+            int cycle = 1;
+            public void RenderImage(string[] data)
+            {
+                for(int i = 0; i <= 220; i++)
+                {
+                    
+                }
+            }
+            public int GetSignal(string[] data)
+            {
+                int result = 0;
+                foreach(string dat in data)
+                {
+                    if (cycle > 220)
+                        return result;
+                    string[] com = dat.Split(" ");
+                    if (com[0] =="noop")
+                    {
+                        cycle++;
+                        if((cycle+20)%40==0)
+                        {
+                            result+=(reg1*cycle);
+                        }                            
+                    }
+                    else
+                    {
+                        if ((cycle + 21) % 40 == 0)
+                        {
+                            result += (reg1 * (cycle+1));
+                        }
+                        cycle += 2;
+                        reg1 += int.Parse(com[1]);
+                        if ((cycle + 20) % 40 == 0)
+                        {
+                            result += (reg1 * cycle);
+                        }
+                    }
+                }
+                return result;
+            }
+
+
+
+        }
+    }
+
+}
--- AdventOfTests/2022_Tests/Day05_Tests/Day05_Test.cs
+++ AdventOfTests/2022_Tests/Day05_Tests/Day05_Test.cs
@@ -1,38 +1,38 @@
-﻿using AdventOfCode.Year2023;
+﻿using AdventOfCode.Year2022;
 
-namespace AdventOfTests._2023_Tests
+namespace AdventOfTests._2022_Tests
 {
-    public class Day01_Test
+    public class Day05_Test
     {
         Day day;
         [SetUp]
         public void Setup()
         {
-            day = new DayXX(0);
+            day = new Day05(5);
         }
         [Test]
         public void RunPartOneSample()
         {
             day.RunPart1(ArgumentType.Sample);
-            Assert.That(day.result, Is.EqualTo(""));
+            Assert.That(day.result, Is.EqualTo("CMZ"));
         }
         [Test]
         public void RunPartOneFull()
         {
             day.RunPart1(ArgumentType.Full);
-            Assert.That(day.result, Is.EqualTo(""));
+            Assert.That(day.result, Is.EqualTo("VCTFTJQCG"));
         }
         [Test]
         public void RunPartTwoSample()
         {
             day.RunPart2(ArgumentType.Sample);
-            Assert.That(day.result, Is.EqualTo(""));
+            Assert.That(day.result, Is.EqualTo("MCD"));
         }
         [Test]
         public void RunPartTwoFull()
         {
             day.RunPart2(ArgumentType.Full);
-            Assert.That(day.result, Is.EqualTo(""));
+            Assert.That(day.result, Is.EqualTo("GCFGLDNJZ"));
         }
 
     }
