0ca0ed29e8682a577d6681c62af48f8eecf8a858
HarrySimpson-KDOR <caniflysouthtoo@gmail.com>
2023-12-06 07:23:16 -0600

    Day6 brute forced lol

---
+++ AdventOfCode/2023/Day06/Day06.cs
@@ -0,0 +1,64 @@
+﻿
+namespace AdventOfCode.Year2023
+{
+    public class Day06 : Day
+    {
+        public Day06(int today, int year) : base(today, year)
+        {
+            
+        }
+        public override void RunPart1(ArgumentType argumentType)
+        {
+            string[] data = argumentType == ArgumentType.Sample ? Sample : Full;
+            RaceCalculator raceCalculator = new RaceCalculator();
+            result = raceCalculator.WaysToWin(data).ToString();
+            Console.WriteLine(result);
+        }
+        public override void RunPart2(ArgumentType argumentType)
+        {
+            string[] data = argumentType == ArgumentType.Sample ? Sample : Full;
+            RaceCalculator raceCalculator = new RaceCalculator();
+            result = raceCalculator.WaysToWin(data, true).ToString();
+            Console.WriteLine(result);
+        }
+        public class RaceCalculator
+        {
+            public long WaysToWin(string[] data, bool correction = false)
+            {
+                List<long> Times = data[0].Split(":")[1].Trim().Split(" ").Where(x=>x!="").Select(x => long.Parse(x)).ToList();
+                List<long> Distances = data[1].Split(":")[1].Trim().Split(" ").Where(x => x != "").Select(x => long.Parse(x)).ToList();
+                if(correction)
+                {
+                    Times = new();
+                    Times.Add(long.Parse(data[0].Split(":")[1].Trim().Replace(" ", "")));
+                    Distances = new();
+                    Distances.Add(long.Parse(data[1].Split(":")[1].Trim().Replace(" ", "")));
+                }
+                long n = Times.Count;
+                long result = 1;
+                long ways = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    bool win = false;
+                    for (long speed = Times[i]; speed >=  0; speed--)
+                    {
+                        long distance = speed * (Times[i] - speed);
+                        if (distance > Distances[i])
+                        {
+                            win = true;
+                            ways++;
+                        }
+                        else if (win)
+                        {
+                            break;
+                        }
+                    }
+                    result *= ways;
+                    ways = 0;
+                }
+                return result;
+            }
+        }
+    }
+
+}
---
+++ AdventOfTests/2023_Tests/Day06_Tests/Day06_Test.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Year2023;
+
+namespace AdventOfTests._2023_Tests
+{
+    public class Day06_Test
+    {
+        Day day;
+        [SetUp]
+        public void Setup()
+        {
+            day = new Day06(6, 2023);
+        }
+        [Test]
+        public void RunPartOneSample()
+        {
+            day.RunPart1(ArgumentType.Sample);
+            Assert.That(day.result, Is.EqualTo("288"));
+        }
+        [Test]
+        public void RunPartOneFull()
+        {
+            day.RunPart1(ArgumentType.Full);
+            Assert.That(day.result, Is.EqualTo("2612736"));
+        }
+        [Test]
+        public void RunPartTwoSample()
+        {
+            day.RunPart2(ArgumentType.Sample);
+            Assert.That(day.result, Is.EqualTo("71503"));
+        }
+        [Test]
+        public void RunPartTwoFull()
+        {
+            day.RunPart2(ArgumentType.Full);
+            Assert.That(day.result, Is.EqualTo("29891250"));
+        }
+
+    }
+}
