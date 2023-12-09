a4d8d2790cec7fad9a03de3c149a421db35b548c
Harry Simpson <Harrymsimpson@gmail.com>
2023-12-09 07:22:52 -0600

    Day 8 and 9

--- AdventOfTests/2023_Tests/Day09_Tests/Day09_Test.cs
+++ AdventOfTests/2023_Tests/Day09_Tests/Day09_Test.cs
@@ -2,37 +2,37 @@
 
 namespace AdventOfTests._2023_Tests
 {
-    public class Day07_Test
+    public class Day09_Test
     {
         Day day;
         [SetUp]
         public void Setup()
         {
-            day = new Day07(7, 2023);
+            day = new Day09(9, 2023);
         }
         [Test]
         public void RunPartOneSample()
         {
             day.RunPart1(ArgumentType.Sample);
-            Assert.That(day.result, Is.EqualTo("6440"));
+            Assert.That(day.result, Is.EqualTo("114"));
         }
         [Test]
         public void RunPartOneFull()
         {
             day.RunPart1(ArgumentType.Full);
-            Assert.That(day.result, Is.EqualTo("255048101"));
+            Assert.That(day.result, Is.EqualTo("1743490457"));
         }
         [Test]
         public void RunPartTwoSample()
         {
             day.RunPart2(ArgumentType.Sample);
-            Assert.That(day.result, Is.EqualTo("5905"));
+            Assert.That(day.result, Is.EqualTo("2"));
         }
         [Test]
         public void RunPartTwoFull()
         {
             day.RunPart2(ArgumentType.Full);
-            Assert.That(day.result, Is.EqualTo("253718286"));
+            Assert.That(day.result, Is.EqualTo("1053"));
         }
 
     }
