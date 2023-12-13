fc1fa316c4a0794fb3e314f609785044cc472858
Harry Simpson <Harrymsimpson@gmail.com>
2023-12-13 07:38:46 -0600

    day 13 + cleanup

--- AdventOfTests/2023_Tests/Day13_Tests/Day13_Test.cs
+++ AdventOfTests/2023_Tests/Day13_Tests/Day13_Test.cs
@@ -1,38 +1,38 @@
-﻿using AdventOfCode.Year2022;
+﻿using AdventOfCode.Year2023;
 
-namespace AdventOfTests._2022_Tests
+namespace AdventOfTests._2023_Tests
 {
-    public class DayXX_Test
+    public class Day13_Test
     {
         Day day;
         [SetUp]
         public void Setup()
         {
-            day = new DayXX(0);
+            day = new Day13(13, 2023);
         }
         [Test]
         public void RunPartOneSample()
         {
             day.RunPart1(ArgumentType.Sample);
-            Assert.That(day.result, Is.EqualTo(""));
+            Assert.That(day.result, Is.EqualTo("405"));
         }
         [Test]
         public void RunPartOneFull()
         {
             day.RunPart1(ArgumentType.Full);
-            Assert.That(day.result, Is.EqualTo(""));
+            Assert.That(day.result, Is.EqualTo("28651"));
         }
         [Test]
         public void RunPartTwoSample()
         {
             day.RunPart2(ArgumentType.Sample);
-            Assert.That(day.result, Is.EqualTo(""));
+            Assert.That(day.result, Is.EqualTo("400"));
         }
         [Test]
         public void RunPartTwoFull()
         {
             day.RunPart2(ArgumentType.Full);
-            Assert.That(day.result, Is.EqualTo(""));
+            Assert.That(day.result, Is.EqualTo("25450"));
         }
 
     }
