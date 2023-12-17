fd66db11c9de7b33221a985a7894dc7b25d356ee
Harry Simpson <Harrymsimpson@gmail.com>
2023-12-17 07:42:18 -0600

    Day 17

---
+++ AdventOfCode/Common/Directions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Common
+{
+    public static class Directions
+    {
+        public static (int x, int y) North = (-1, 0);
+        public static (int x, int y) South = (1, 0);
+        public static (int x, int y) East = (0, 1);
+        public static (int x, int y) West = (0, -1);
+        public static (int x, int y) Invert((int x, int y) direction)
+        {
+            return (direction.x * -1, direction.y * -1);
+        }
+    }
+}
---
+++ AdventOfTests/2023_Tests/Day17_Tests/Day17_Test.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Year2023;
+
+namespace AdventOfTests._2023_Tests
+{
+    public class Day17_Test
+    {
+        Day day;
+        [SetUp]
+        public void Setup()
+        {
+            day = new Day17(17, 2023);
+        }
+        [Test]
+        public void RunPartOneSample()
+        {
+            day.RunPart1(ArgumentType.Sample);
+            Assert.That(day.result, Is.EqualTo("102"));
+        }
+        [Test]
+        public void RunPartOneFull()
+        {
+            day.RunPart1(ArgumentType.Full);
+            Assert.That(day.result, Is.EqualTo("1008"));
+        }
+        [Test]
+        public void RunPartTwoSample()
+        {
+            day.RunPart2(ArgumentType.Sample);
+            Assert.That(day.result, Is.EqualTo("94"));
+        }
+        [Test]
+        public void RunPartTwoFull()
+        {
+            day.RunPart2(ArgumentType.Full);
+            Assert.That(day.result, Is.EqualTo("1210"));
+        }
+
+    }
+}
